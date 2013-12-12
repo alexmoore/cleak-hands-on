08e3897f69a51e623ddb5c6ad84ff7bf92334664
Alex Moore <moore.alex@gmail.com>
2013-12-12 16:10:49 -0500

    Initial commit

---
+++ import.rb
@@ -0,0 +1,20 @@
+require 'json'
+require 'riak'
+
+file = open('data/ErlangInfo.json')
+json = file.read
+
+tweets = JSON.parse(json)
+
+client = Riak::Client.new(:protocol => "pbc", :pb_port => 10017)
+
+tweet_bucket = client.bucket('tweets')
+
+tweets.each do |tweet|
+	tweet_obj = tweets_bucket.new(tweet[:id_str])
+	tweet_obj.data = tweet
+
+	
+
+	tweet_obj.store()
+end
